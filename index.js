ab3a6abc541094564e4f4215610cf1018b3f62e3
craftycram <github@craftycram.net>
2019-12-03 15:56:48 +0100

    print invader function

--- index.js
+++ index.js
@@ -46,23 +46,30 @@ jimp.read('prag.jpg', (err, map_image) => {
 */
 
 
+let i = 0;
+const invader = generateInvader(6, 14, i, 5);
+
 //Vorerst nur ein Platzhalter
 setInterval(function () {
 
   clearConsole()
   const xpos = Math.floor(Math.random() * 150);
   const ypos = Math.floor(Math.random() * 70);
-  generateInvader(6, 4, xpos, ypos);
+  //generateInvader(6, 4, xpos, ypos);
   //generateInvader(6, 14, xpos, ypos);
+  printInvader(invader, xpos, ypos);
   rl.cursorTo(process.stdout, 0, 0);
   console.log(xpos, ypos);
+  i++;
+
 
 }, 1000);
+
 
 /*
  * HELPER FUNCTIONS - DO NOT CHANGE
  */
-function clearConsole () {
+function clearConsole() {
   const blank = '\n'.repeat(process.stdout.rows)
   console.log(blank)
 
@@ -70,41 +77,44 @@ function clearConsole () {
   rl.clearScreenDown(process.stdout)
 }
 
-function writeCharacterToConsole (char, x, y) {
+function writeCharacterToConsole(char, x, y) {
   rl.cursorTo(process.stdout, x, y)
   process.stdout.write(char)
 }
 
-function generateInvader (width, height, xpos, ypos) {
+function generateInvader(width, height, xpos, ypos) {
 
   let invader = [];
   //const printChar = '#';
   const printChar = '■';
   //const printChar = '👾';
 
+  const rand = Math.random();
+
+
   for (let x = 0; x < width; x++) {
 
     for (let y = 0; y < height; y++) {
 
-      const rand = Math.random();
       const color_r = Math.random() * 255;
       const color_g = Math.random() * 255;
       const color_b = Math.random() * 255;
 
       const hue = Math.random() * 360;
 
-      if (rand > 0.5) {
+      const randProcecced = rand.toString()[x * y];
+
+
+      if (parseInt(randProcecced) > 5) {
 
         const colorCharRGB = chalk.rgb(color_r, color_g, color_b)(printChar);
         const colorCharHSV = chalk.hsv(hue, 100, 100)(printChar);
-        /*
-        rl.cursorTo(process.stdout, 0, height + 1);
-        console.log(color_r, color_g, color_b);
-        */
+
 
 
         writeCharacterToConsole((colorCharHSV), x + xpos, y + ypos);
         writeCharacterToConsole((colorCharHSV), width * 2 - x + xpos, y + ypos);
+
 
         if (!Array.isArray(invader[x])) {
           invader[x] = [];
@@ -132,10 +142,23 @@ function generateInvader (width, height, xpos, ypos) {
 
   }
 
-  /*
-  rl.cursorTo(process.stdout, 0, height + 2);
-  console.log(invader);
-  */
+  return invader;
 
+}
+
+function printInvader (invader, xpos, ypos) {
+
+  for (let x = 0; x < invader.length; x++) {
+
+    for (let y = 0; y < invader[0].length; y++) {
+
+      if (invader[x][y] == 1) {
+        writeCharacterToConsole('#', x + xpos, y + ypos);
+        writeCharacterToConsole('#', invader.length * 2 - x + xpos, y + ypos);
+      }
+      
+    }
+    
+  }
 
 }
